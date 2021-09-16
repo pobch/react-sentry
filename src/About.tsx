3ee162196ff474f900b9206df77bd4e235051532
Pob <590650@gmail.com>
2021-09-16 14:39:44 +0700

    Try logging to Sentry with level

--- src/About.tsx
+++ src/About.tsx
@@ -1,8 +1,11 @@
 import { useEffect } from 'react'
+import * as Sentry from '@sentry/react'
+import { Severity } from '@sentry/react'
 
 async function mockApi(ms: number) {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
+      Sentry.captureMessage('[log at unhandled prom] warning', Severity.Warning)
       reject(new Error('[unhandle prom] api response error'))
     }, ms)
   })
