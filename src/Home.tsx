62cb2a97038cb6f72a9341befaa46d3a28e6aa7e
Pob <590650@gmail.com>
2021-10-10 00:33:39 +0700

    Add setTimeout error

--- src/Home.tsx
+++ src/Home.tsx
@@ -2,13 +2,26 @@ export function Home() {
   return (
     <div>
       <h1>Home</h1>
-      <button
-        onClick={() => {
-          throw new Error('[unhandle excep] onClick error')
-        }}
-      >
-        Break
-      </button>
+      <p>
+        <button
+          onClick={() => {
+            throw new Error('[unhandle excep] onClick error')
+          }}
+        >
+          Break
+        </button>
+      </p>
+      <p>
+        <button
+          onClick={() => {
+            setTimeout(() => {
+              throw new Error('[unhandle excep] setTimeout error')
+            }, 2000)
+          }}
+        >
+          Delay Break
+        </button>
+      </p>
     </div>
   )
 }
